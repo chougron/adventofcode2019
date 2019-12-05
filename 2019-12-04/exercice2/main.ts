5aa9ebc278bfec4f3a7dde7f92c11231fed50abd
Camille Hougron <camille.hougron@pixpay.fr>
2019-12-05 17:04:40 +0100

    Exercise 2 for 2019-12-04

--- 2019-12-04/exercice2/main.ts
+++ 2019-12-04/exercice2/main.ts
@@ -8,11 +8,11 @@
       chars[2] <= chars[3] &&
       chars[3] <= chars[4] &&
       chars[4] <= chars[5] && (
-      chars[0] === chars[1] ||
-      chars[1] === chars[2] ||
-      chars[2] === chars[3] ||
-      chars[3] === chars[4] ||
-      chars[4] === chars[5] )
+      (chars[0] === chars[1] && chars[1] !== chars[2]) ||
+      (chars[1] === chars[2] && chars[2] !== chars[3] && chars[1] !== chars[0]) ||
+      (chars[2] === chars[3] && chars[3] !== chars[4] && chars[2] !== chars[1]) ||
+      (chars[3] === chars[4] && chars[4] !== chars[5] && chars[3] !== chars[2]) ||
+      (chars[4] === chars[5] && chars[4] !== chars[3]) )
       ) {
         numbers++;
       }
