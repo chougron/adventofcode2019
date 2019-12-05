4034a8357cc24343975495c148587e29e2a1dc89
Camille Hougron <camille.hougron@pixpay.fr>
2019-12-05 14:46:11 +0100

    Exercise 1 for 2019-12-04

---
+++ 2019-12-04/exercice1/main.ts
@@ -0,0 +1,19 @@
+let  numbers = 0;
+for (let combination = 278384; combination <= 824795; combination++){
+  const chars = `${combination}`.split('');
+  if (
+    chars[0] <= chars[1] &&
+    chars[1] <= chars[2] &&
+    chars[2] <= chars[3] &&
+    chars[3] <= chars[4] &&
+    chars[4] <= chars[5] && (
+    chars[0] === chars[1] ||
+    chars[1] === chars[2] ||
+    chars[2] === chars[3] ||
+    chars[3] === chars[4] ||
+    chars[4] === chars[5] )
+    ) {
+      numbers++;
+    }
+}
+console.log(numbers);
